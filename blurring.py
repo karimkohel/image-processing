f98635bb830b85642f5947d2d717e314843114cf
karimkohel <kareemkohel@gmail.com>
2019-11-02 18:39:53 +0200

    added blurring

--- blurring.py
+++ blurring.py
@@ -17,9 +17,21 @@ while True:
 	mask = cv2.inRange(hsv, lowerPink, upperPink)
 	res = cv2.bitwise_and(frame, frame, mask=mask)
 
+	# kernel = np.ones((15,15), np.float32) / 225
+	# smoothed = cv2.filter2D(res, -1, kernel)
+
+	# blur = cv2.GaussianBlur(res, (15,15), 0)
+
+	median = cv2.medianBlur(res, 15)
+
 	cv2.imshow("frame", frame)
-	cv2.imshow("mask", mask)
+	# cv2.imshow("mask", mask) dont need here
 	cv2.imshow("res", res)
+	# cv2.imshow("smoothed", smoothed)
+	# cv2.imshow("blur", blur)
+	cv2.imshow("median", median)
+
+
 
 	k = cv2.waitKey(5) & 0xFF
 	if k == 27:
