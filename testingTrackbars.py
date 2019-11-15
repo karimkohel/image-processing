b8d73863009e4f3333cee9866aeebbb56a9c7133
karim kohel <kareemkohel@gmail.com>
2019-11-15 09:47:50 +0200

    removed hazem file

--- testingTrackbars.py
+++ testingTrackbars.py
@@ -3,31 +3,48 @@
 import cv2
 import numpy as np
 
-def nothing(x):
-	pass
+def nth(x):
+	print(x)
+
 
 cap = cv2.VideoCapture(0)
 
-cv2.namedWindow("test1")
+cv2.namedWindow("Trackbars")
 
-cv2.createTrackbar('test', 'test1', 50, 500, nothing)
-cv2.createTrackbar("colorToGray", 'test1', 0, 1, nothing)
+cv2.createTrackbar('L-H', "Trackbars", 50, 179, nth)
+cv2.createTrackbar('L-S', "Trackbars", 50, 255, nth)
+cv2.createTrackbar('L-V', "Trackbars", 50, 255, nth)
+cv2.createTrackbar('U-H', "Trackbars", 50, 179, nth)
+cv2.createTrackbar('U-S', "Trackbars", 50, 255, nth)
+cv2.createTrackbar('U-V', "Trackbars", 50, 255, nth)
+
 
 while True:
 	_, frame = cap.read()
 
-	test = cv2.getTrackbarPos('test', 'test1')
-	test2 = cv2.getTrackbarPos("colorToGray", 'test1')
-
-	if test2 == 1:
-		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
 
-	font = cv2.FONT_HERSHEY_COMPLEX
-	cv2.putText(frame, str(test), (50,200), font, 1, (0,255,0))
+	LH = cv2.getTrackbarPos('L-H', 'Trackbars')
+	LS = cv2.getTrackbarPos('L-S', 'Trackbars')
+	LV = cv2.getTrackbarPos('L-V', 'Trackbars')
+	UH = cv2.getTrackbarPos('U-H', 'Trackbars')
+	US = cv2.getTrackbarPos('U-S', 'Trackbars')
+	UV = cv2.getTrackbarPos('U-V', 'Trackbars')
+
+
+
+	min = np.array([LH,LS,LV])
+	max = np.array([UH,US,UV])
+
+	mask = cv2.inRange(hsv, min, max)
+
+	res = cv2.bitwise_and(frame, frame,mask=mask)
+
 
 	cv2.imshow("original", frame)
-
+	# cv2.imshow("mask", mask)
+	cv2.imshow("result", res)
 	key = cv2.waitKey(1)
 	if key == 27:
 		break
