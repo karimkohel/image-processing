3ef82b3ee0f8d69aa77aef732de1d6af0886bbdb
karim kohel <kareemkohel@gmail.com>
2020-01-29 11:50:14 +0100

    that too

--- feed.py
+++ feed.py
@@ -3,13 +3,17 @@
 import cv2
 
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 while True:
 	ret, frame = cap.read()
 	cv2.imshow('frame', frame)
 
 	if cv2.waitKey(1) & 0xFF == ord('q'):
+		cv2.imwrite("fingers.png", frame)
+		break
+	elif cv2.waitKey(1) & 0xFF == ord('s'):
+		cv2.imwrite("fingers.png", frame)
 		break
 
 cap.release()
